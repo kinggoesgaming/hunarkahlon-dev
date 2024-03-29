c6b144a43ff5ce00448c7f893751d1ab9a2bc9a8
Hunar Roop Kahlon <hunar.roop@gmail.com>
2024-03-29 16:29:40 -0700

    build(config,remix,vite): turn `remix` into `spa` mode
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+import { copyFileSync } from 'node:fs'
+import { join } from 'node:path'
+
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
@@ -6,5 +9,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  base: '/hunarkahlon-dev/',
+  plugins: [remix({
+    basename: '/hunarkahlon-dev/',
+    buildEnd({ viteConfig: { build: { outDir }, isProduction, }}) {
+      if (!isProduction) {
+        return
+      }
+
+      copyFileSync(
+        join(outDir, 'index.html'),
+        join(outDir, '404.html'),
+      )
+    },
+    ssr: false,
+  }), tsconfigPaths()],
 });
