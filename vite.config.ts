98aaad8780146ae01f9f38b0a08afd312a648c0e
Hunar Roop Kahlon <hunar.roop@gmail.com>
2024-03-29 17:43:11 -0700

    build(config,vite): remove `tsconfig-paths` plugin
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,30 @@
 import { copyFileSync } from 'node:fs'
 import { join } from 'node:path'
 
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
+import { defineConfig } from 'vite'
 
-installGlobals();
+installGlobals()
 
 export default defineConfig({
   base: '/hunarkahlon-dev/',
-  plugins: [remix({
-    basename: '/hunarkahlon-dev/',
-    buildEnd({ viteConfig: { build: { outDir }, isProduction, }}) {
-      if (!isProduction) {
-        return
-      }
+  plugins: [
+    remix({
+      basename: '/hunarkahlon-dev/',
+      buildEnd({
+        viteConfig: {
+          build: { outDir },
+          isProduction,
+        },
+      }) {
+        if (!isProduction) {
+          return
+        }
 
-      copyFileSync(
-        join(outDir, 'index.html'),
-        join(outDir, '404.html'),
-      )
-    },
-    ssr: false,
-  }), tsconfigPaths()],
-});
+        copyFileSync(join(outDir, 'index.html'), join(outDir, '404.html'))
+      },
+      ssr: false,
+    }),
+  ],
+})
